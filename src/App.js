1cb2b8862bacc7e2a58eafee5a641d76c77f57cf
Teemu Sarapisto <tsarpf@gmail.com>
2017-04-20 19:09:02 +0300

    Use baret instead of normal React

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react'
+import React from 'baret'
 import logo from './logo.svg'
 import styles from './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className={styles.App}>
-        <div className={styles.header}>
-          <img src={logo} className={styles.logo} alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className={styles.intro}>
-          test
-        </p>
-      </div>
-    )
-  }
-}
+const App = () =>
+  <div className={styles.App}>
+    <div className={styles.header}>
+      <img src={logo} className={styles.logo} alt="logo" />
+      <h2>Welcome to React</h2>
+    </div>
+    <p className={styles.intro}>
+      test
+    </p>
+  </div>
+
 
 export default App
