ad7e044a4fab0bf3fa4e9552356f7ba057db2d8c
Teemu Sarapisto <tsarpf@gmail.com>
2017-04-21 01:57:36 +0300

    Use front-end file server as socket server

--- src/util/socket.js
+++ src/util/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:3001')
+const socket = io(`http://${location.hostname}:3001`)
 
 socket.on('connect', () => console.log('socket.io connected'));
 socket.on('disconnect', () => console.log('socket.io disconnected'))
