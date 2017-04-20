6d4146288a23d8e0047a16518121b129e6ead96a
Teemu Sarapisto <tsarpf@gmail.com>
2017-04-20 23:25:10 +0300

    Add game state indicator

--- src/components/rps/component.js
+++ src/components/rps/component.js
@@ -4,30 +4,38 @@ import {addListener, newEmitter} from '../../util/socket.js'
 
 
 const move = newEmitter('move')
-const gameStream = Bacon.fromBinder(sink => addListener('game', sink))
-const stateStream = Bacon.fromBinder(sink => addListener('state', sink))
-                         .map(state => {
-                           switch (state) {
-                             case 'new game':
-                               return 'Game started!'
-                             case 'waiting':
-                               return 'Waiting for another player... Please wait'
-                             default:
-                               return 'Something is hopefully happening...'
-                           }
-                         })
 
+const moveBus = new Bacon.Bus()
+
+const moveStream = moveBus
+  .map(m => move(m))
+
+const stateMap = {
+  'new game': 'Game started',
+  'waiting': 'Waiting for an opponent...'
+}
+
+const stateStream = Bacon
+  .fromBinder(sink => addListener('state', sink))
+
+const combineStateStream = Bacon
+  .when([moveStream, stateStream.toProperty()],
+        (move, state) => state === 'new game' ? 'Waiting for opponent to make a move' : null,
+        [stateStream], state => stateMap[state])
+  .filter(e => e)
+
+const gameStream = Bacon.fromBinder(sink => addListener('game', sink))
 const RPS = () =>
   <div>
     <div>
-      {stateStream}
+      {combineStateStream}
     </div>
     <div>
       {gameStream}
     </div>
-    <button onClick={() => move(0)}> Rock </button>
-    <button onClick={() => move(1)}> Paper </button>
-    <button onClick={() => move(2)}> Scissors </button>
+    <button onClick={() => moveBus.push(0)}> Rock </button>
+    <button onClick={() => moveBus.push(1)}> Paper </button>
+    <button onClick={() => moveBus.push(2)}> Scissors </button>
   </div>
 
 export default RPS
